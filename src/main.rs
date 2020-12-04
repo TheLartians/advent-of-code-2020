495f0ac591e6bd55aa3bfa92961599607ac03411
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-04 12:39:19 +0100

    simplify solution

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::env;
 use std::fs;
 
 fn is_in_range(input: &str, unit: &str, min: i32, max: i32) -> bool {
-   Regex::new(&format!(r"^(\d+|[a-f]){}$", unit))
+  Regex::new(&format!(r"^(\d+|[a-f]){}$", unit))
     .unwrap()
     .captures(input)
-    .and_then(|m| Some(m[1].parse::<i32>().and_then(|v| Ok(v >= min && v <= max))))
-    .unwrap_or(Ok(false))
-    .unwrap()
+    .and_then(|m| m[1].parse::<i32>().ok())
+    .and_then(|v| Some(v >= min && v <= max))
+    .unwrap_or(false)
 }
 
 fn is_valid_pp_entry(key: &str, input: &str) -> bool {
@@ -19,7 +19,9 @@ fn is_valid_pp_entry(key: &str, input: &str) -> bool {
     "eyr" => is_in_range(input, "", 2020, 2030),
     "hgt" => is_in_range(input, "cm", 150, 193) || is_in_range(input, "in", 59, 76),
     "hcl" => Regex::new(r"^#(\d|[a-f]){6}$").unwrap().is_match(input),
-    "ecl" => Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap().is_match(input),
+    "ecl" => Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$")
+      .unwrap()
+      .is_match(input),
     "pid" => Regex::new(r"^\d{9}$").unwrap().is_match(input),
     _ => false,
   }
@@ -27,14 +29,12 @@ fn is_valid_pp_entry(key: &str, input: &str) -> bool {
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  println!("reading file {:?}", args[1]);
-
   let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
   let passports: Vec<&str> = contents.split("\n\n").collect();
   let separator = Regex::new(r"(\n| )").expect("Invalid regex");
 
   let mut valid = 0;
-  let expected = 7;
+  let required_field_count = 7;
 
   for passport in passports {
     // parse input and filter valid properties
@@ -42,19 +42,23 @@ fn main() {
       .split(passport)
       .filter(|s| s.len() > 0)
       .map(|s| s.split(":").collect::<Vec<&str>>())
-      .filter(|v| v.len() == 2 && is_valid_pp_entry(v[0], v[1]))
-      .map(|v| (v[0], v[1]))
+      .filter_map(|v| match v.len() == 2 && is_valid_pp_entry(v[0], v[1]) {
+        true => Some((v[0], v[1])),
+        false => None,
+      })
       .collect();
 
     // prevent duplicate entries from beeing counted
     let mut unique: HashSet<&str> = HashSet::new();
-
     for part in parts {
       unique.insert(part.0);
     }
-    if unique.len() == expected {
+
+    // check if all required fields are there
+    if unique.len() == required_field_count {
       valid = valid + 1;
     }
   }
+
   println!("Counted {:?} valid passports", valid)
 }
