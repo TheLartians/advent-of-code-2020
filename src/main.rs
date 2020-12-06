b98d137bc58fd7a9262ad6b0bf6c71483953c815
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-06 10:11:26 +0100

    solve day 6

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::env;
 use std::fs;
 
 fn get_questions_in_group(group: &str) -> usize {
-  let mut questions = HashSet::new();
-  for person in group.split('\n') {
-    for question in person.chars() {
-      questions.insert(question);
-    }
+  let mut people = group.split('\n').filter(|v| v.len() > 0);
+  let first = people.next().unwrap();
+  let mut questions: HashSet<u8> = first.bytes().collect();
+  for person in people {
+    questions = person.bytes().filter(|c| questions.contains(c)).collect();
   }
   return questions.len();
 }
@@ -16,10 +16,14 @@ fn main() {
   let args: Vec<String> = env::args().collect();
   let input = fs::read_to_string(&args[1]).unwrap();
 
-  let question_count = input.split("\n\n")
-  .filter(|s| s.len() > 0)
-  .map(|s| get_questions_in_group(s))
-  .fold(0, |a,b| a+b);
-  
-  println!("Number of questions with at least one 'yes': {}", question_count)
+  let question_count = input
+    .split("\n\n")
+    .filter(|s| s.len() > 0)
+    .map(|s| get_questions_in_group(s))
+    .fold(0, |a, b| a + b);
+
+  println!(
+    "Questions where everyone answered 'yes': {}",
+    question_count
+  )
 }
