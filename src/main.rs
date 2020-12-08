2be02e854c7b37441ee3bbeaf1a75b40aa0b81bc
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-08 11:03:53 +0100

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,52 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::string::String;
 
-fn parse_rule_part(input: &str) -> (String, u32) {
-  lazy_static! {
-    static ref RULE_PART_REGEX: Regex = Regex::new(r"^(\d) ([a-z ]*) (?:bag|bags)$").unwrap();
-  }
-
-  let capture = RULE_PART_REGEX.captures(input).unwrap();
-
-  return (
-    String::from(capture.get(2).unwrap().as_str()),
-    capture.get(1).unwrap().as_str().parse::<u32>().unwrap(),
-  );
+enum Instruction {
+  ACC,
+  JMP,
+  NOP,
 }
 
-fn parse_rule(input: &str) -> (String, Vec<(String, u32)>) {
-  lazy_static! {
-    static ref RULE_REGEX: Regex = Regex::new(r"^([a-z ]*) bags contain ([\da-z, ]*).$").unwrap();
-  }
-
-  let capture = RULE_REGEX.captures(input).unwrap();
-  let name = String::from(capture.get(1).unwrap().as_str());
-  let content_definitions = capture.get(2).unwrap().as_str();
-
-  let content = content_definitions
-    .split(", ")
-    .filter(|s| *s != "no other bags")
-    .map(|s| parse_rule_part(s))
-    .collect();
-  return (name, content);
+fn parse_instruction(input: &str) -> (Instruction, i32) {
+  let mut parts = input.split(' ');
+  let instruction = match parts.next().unwrap() {
+    "acc" => Instruction::ACC,
+    "jmp" => Instruction::JMP,
+    _ => Instruction::NOP,
+  };
+  let value = parts.next().unwrap().parse::<i32>().unwrap();
+  return (instruction, value);
 }
 
-fn add_inner_bags<'a>(
-  res: &mut HashMap<&'a str, u32>,
-  item: &'a str,
-  rules: &'a HashMap<String, Vec<(String, u32)>>,
-) -> u32 {
-  let mut total = 1;
+fn run_until_loop(instructions: &Vec<(Instruction, i32)>) -> i32 {
+  let mut accumulator = 0;
+  let mut position = 0;
+  let mut history: Vec<bool> = instructions.iter().map(|_| false).collect();
 
-  match rules.get(item) {
-    Some(rule) => {
-      for (color, inner_count) in rule {
-        match res.get(color as &str) {
-          Some(content_count) => {
-            total += inner_count * content_count;
-          }
-          None => total += inner_count * add_inner_bags(res, color, rules),
+  loop {
+    if history[position] {
+      break;
+    } else {
+      history[position] = true;
+      let (instruction, value) = &instructions[position];
+      match instruction {
+        Instruction::ACC => {
+          accumulator += value;
+          position += 1;
+        }
+        Instruction::JMP => {
+          position = position.wrapping_add(*value as usize);
+        }
+        Instruction::NOP => {
+          position += 1;
         }
       }
     }
-    None => {}
   }
 
-  res.insert(&item, total);
-  return total;
-}
-
-fn get_inner_bag_count(item: &str, rules: &HashMap<String, Vec<(String, u32)>>) -> u32 {
-  let mut res = HashMap::new();
-  return add_inner_bags(&mut res, item, rules) - 1;
+  return accumulator;
 }
 
 fn main() {
@@ -72,15 +55,15 @@ fn main() {
   let filename: String = args.next().unwrap();
   let input = io::BufReader::new(File::open(filename).unwrap());
 
-  let rules: HashMap<String, Vec<(String, u32)>> = input
+  let instructions: Vec<(Instruction, i32)> = input
     .lines()
     .filter_map(|s| s.ok())
     .filter(|s| s.len() > 0)
-    .map(|s| parse_rule(&s))
+    .map(|s| parse_instruction(&s))
     .collect();
 
   println!(
-    "shiny gold contains {} other bags.",
-    get_inner_bag_count("shiny gold", &rules)
+    "the program repeated the loop with {}",
+    run_until_loop(&instructions)
   );
 }
