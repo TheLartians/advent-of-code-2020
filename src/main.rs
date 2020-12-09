38543a98d6ae145794c3a1d0c0a2a88da7211080
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-09 09:29:39 +0100

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -3,83 +3,32 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::string::String;
 
-enum Instruction {
-  ACC,
-  JMP,
-  NOP,
-}
-
-fn parse_instruction(input: &str) -> (Instruction, i32) {
-  let mut parts = input.split(' ');
-  let instruction = match parts.next().unwrap() {
-    "acc" => Instruction::ACC,
-    "jmp" => Instruction::JMP,
-    _ => Instruction::NOP,
-  };
-  let value = parts.next().unwrap().parse::<i32>().unwrap();
-  return (instruction, value);
-}
-
-fn run_bytecode<'a>(instructions: &'a Vec<(Instruction, i32)>) -> Result<i32, i32> {
-  let mut accumulator = 0;
-  let mut position = 0;
-  let mut history: Vec<bool> = instructions.iter().map(|_| false).collect();
-
-  loop {
-    if position == instructions.len() {
-      return Ok(accumulator);
-    } else if history[position] {
-      return Err(accumulator);
-    } else {
-      history[position] = true;
-      let (instruction, value) = &instructions[position];
-      match instruction {
-        Instruction::ACC => {
-          accumulator += value;
-          position += 1;
-        }
-        Instruction::JMP => {
-          position = position.wrapping_add(*value as usize);
-        }
-        Instruction::NOP => {
-          position += 1;
-        }
-      }
-    }
-  }
-}
-
 fn main() {
   let mut args = env::args();
   args.next();
   let filename: String = args.next().unwrap();
   let input = io::BufReader::new(File::open(filename).unwrap());
+  let preamble_len = args.next().unwrap().parse::<usize>().unwrap();
 
-  let mut instructions: Vec<(Instruction, i32)> = input
+
+  let numbers: Vec<i64> = input
     .lines()
     .filter_map(|s| s.ok())
     .filter(|s| s.len() > 0)
-    .map(|s| parse_instruction(&s))
+    .map(|s|s.parse::<i64>().unwrap())
     .collect();
 
-  let mut result: Result<i32, i32> = Err(0);
-  for i in 0..instructions.len() {
-    match instructions[i].0 {
-      Instruction::JMP => {
-        instructions[i].0 = Instruction::NOP;
-        result = run_bytecode(&instructions);
-        instructions[i].0 = Instruction::JMP;
+  for i in preamble_len..numbers.len() {
+    let mut is_sum = false;
+    let preamble_indices = i-preamble_len..i;
+    for j in preamble_indices.clone() {
+      for k in preamble_indices.clone() {
+        is_sum |= numbers[j] != numbers[k] && numbers[i] == numbers[j] + numbers[k];
       }
-      Instruction::NOP => {
-        instructions[i].0 = Instruction::JMP;
-        result = run_bytecode(&instructions);
-        instructions[i].0 = Instruction::NOP;
-      }
-      _ => {}
     }
-    if let Ok(res) = result {
-      println!("changed instruction at {} and got result {:?}", i, res);
-      break;
+    if !is_sum {
+      println!("the number {} at {} is not a sum of previous {}", numbers[i], i, preamble_len);
     }
   }
+
 }
