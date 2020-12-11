8facecbd10b1f4a8f507f4332b2d9b31cfa0a533
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-11 13:12:07 +0100

    solve part 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use std::cmp;
 use std::env;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::string::String;
 
-fn _print_matrix<T: fmt::Display>(input: &Vec<T>, columns: usize, printer: fn(&T) -> char) {
+fn print_matrix<T: fmt::Display>(input: &Vec<T>, columns: usize, printer: fn(&T) -> char) {
   for i in 0..input.len() {
     print!("{}", printer(&input[i]));
     if (i + 1) % columns == 0 {
@@ -13,26 +14,61 @@ fn _print_matrix<T: fmt::Display>(input: &Vec<T>, columns: usize, printer: fn(&T
   }
 }
 
-fn pad<T: Copy>(input: &mut Vec<T>, value: &T, count: usize) -> Vec<T> {
-  let mut result = vec![*value; count];
-  result.append(input);
-  result.append(&mut vec![*value; count]);
-  return result;
-}
-
 fn count_neighbors(input: &Vec<u8>, rows: usize, columns: usize) -> Vec<u8> {
   let mut result: Vec<u8> = vec![0; input.len()];
-  let get_index = |i: usize, j: usize| i * columns + j;
 
-  for i in 1..rows - 1 {
-    for j in 1..columns - 1 {
-      if input[get_index(i, j)] == b'#' {
-        for ki in i - 1..i + 2 {
-          for kj in j - 1..j + 2 {
-            result[get_index(ki, kj)] += 1;
+  let get_index = |i: usize, j: usize| {
+    if i < columns && j < rows {
+      return Ok(i + j * columns);
+    } else {
+      return Err(());
+    }
+  };
+
+  let set_neighbouring = |res: &mut Vec<u8>, i, j| {
+    if let Ok(idx) = get_index(i, j) {
+      res[idx] += 1;
+      return match input[idx] {
+        b'#' | b'L' => true,
+        _ => false,
+      };
+    } else {
+      return true;
+    }
+  };
+
+  let directions = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+  ];
+
+  let get_directional_index = |i: usize, k: usize, d: i32| match d {
+    1 => i + k,
+    -1 => i.wrapping_sub(k),
+    _ => i,
+  };
+
+  for i in 0..columns {
+    for j in 0..rows {
+      if input[get_index(i, j).unwrap()] == b'#' {
+        let mut visible: Vec<bool> = directions.iter().map(|_| true).collect();
+        for k in 1..cmp::max(rows, columns) {
+          for (vi, (di, dj)) in directions.iter().enumerate() {
+            if visible[vi] {
+              visible[vi] &= !set_neighbouring(
+                &mut result,
+                get_directional_index(i, k, *di),
+                get_directional_index(j, k, *dj),
+              );
+            }
           }
         }
-        result[get_index(i, j)] -= 1;
       }
     }
   }
@@ -45,20 +81,17 @@ fn main() {
   args.next();
   let filename: String = args.next().unwrap();
   let get_input = || io::BufReader::new(File::open(&filename).unwrap());
-  let rows = get_input().lines().count() + 2;
-  let columns = get_input().lines().next().unwrap().unwrap().len() + 2;
+  let rows = get_input().lines().count();
+  let columns = get_input().lines().next().unwrap().unwrap().len();
 
-  let mut state: Vec<u8> = pad(
-    &mut get_input()
-      .lines()
-      .filter_map(|s| s.ok())
-      .map(|s| pad(&mut s.bytes().collect::<Vec<u8>>(), &b'.', 1))
-      .flatten()
-      .collect(),
-    &b'.',
-    columns,
-  );
+  let mut state: Vec<u8> = get_input()
+    .lines()
+    .filter_map(|s| s.ok())
+    .map(|s| s.bytes().collect::<Vec<u8>>())
+    .flatten()
+    .collect();
 
+  // check if input is rectangular
   assert_eq!(state.len(), rows * columns);
 
   let mut step = 0;
@@ -69,14 +102,14 @@ fn main() {
       match state[i] {
         b'L' => {
           if neighbors[i] == 0 {
+            state[i] = b'#';
             changed = true;
-            state[i] = b'#'
           }
         }
         b'#' => {
-          if neighbors[i] >= 4 {
+          if neighbors[i] >= 5 {
+            state[i] = b'L';
             changed = true;
-            state[i] = b'L'
           }
         }
         _ => {}
@@ -92,4 +125,6 @@ fn main() {
       state.iter().filter(|&&v| v == b'#').count()
     );
   }
+
+  print_matrix(&state, columns, |&v| v as char);
 }
