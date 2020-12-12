2364f4dfe15a82bd02c57ffbdb4dca86c1d5066c
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-12 11:11:51 +0100

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,24 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::string::String;
 
-fn is_valid_password(input: &str) -> bool {
-  let mut it = input.split(' ');
-  let policy = it
-    .next()
-    .unwrap()
-    .split('-')
-    .map(|s| s.parse::<usize>().unwrap());
-  let letter = it.next().unwrap().bytes().next().unwrap();
-  let pw = it.next().unwrap();
-
-  return policy.fold(0, |c, i| {
-    if pw.as_bytes()[i - 1] == letter {
-      c + 1
-    } else {
-      c
-    }
-  }) == 1;
-}
-
 fn main() {
   let mut args = env::args();
   args.next();
   let filename: String = args.next().unwrap();
   let get_input = || io::BufReader::new(File::open(&filename).unwrap());
 
-  let valid = get_input()
+  let trees = get_input()
     .lines()
     .filter_map(|s| s.ok())
-    .filter(|s| is_valid_password(s))
-    .count();
+    .map(|s| s.bytes().map(|b| b == b'#').collect::<Vec<bool>>());
 
-  println!("found {} valid passwords", valid);
+  let mut position = 0;
+  let mut collisions = 0;
+  for row in trees {
+    if row[position % row.len()] {
+      collisions += 1;
+    }
+    position += 3;
+  }
+  println!("encountered {} trees", collisions);
 }
