3256b9fe840bd4dcc90f5d217ddacb6c656e76f1
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-12 10:57:21 +0100

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,31 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::string::String;
 
+fn is_valid_password(input: &str) -> bool {
+  let mut it = input.split(' ');
+  let policy: Vec<usize> = it
+    .next()
+    .unwrap()
+    .split('-')
+    .map(|s| s.parse::<usize>().unwrap())
+    .collect();
+  let letter = it.next().unwrap().bytes().next().unwrap();
+  let pw = it.next().unwrap();
+  let count = pw.bytes().filter(|&b| b == letter).count();
+  return count >= policy[0] && count <= policy[1];
+}
+
 fn main() {
   let mut args = env::args();
   args.next();
   let filename: String = args.next().unwrap();
   let get_input = || io::BufReader::new(File::open(&filename).unwrap());
 
-  let mut input: Vec<i64> = get_input()
+  let valid = get_input()
     .lines()
     .filter_map(|s| s.ok())
-    .map(|s| s.parse::<i64>().unwrap())
-    .collect();
-  input.sort();
+    .filter(|s| is_valid_password(s))
+    .count();
 
-  let target = 2020;
-
-  for v1 in &input {
-    for v2 in &input {
-      if input.binary_search(&(target - v1 - v2)).is_ok() {
-        println!("the result is {}", v1 * v2 * (target - v1 - v2));
-        return;
-      }
-    }
-  }
+  println!("found {} valid passwords", valid);
 }
