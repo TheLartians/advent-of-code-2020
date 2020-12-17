1ed6b209e92d401b2325b95e4234d80854515f24
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-17 18:49:26 +0100

    solve part 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use itertools::iproduct;
-use ndarray::Array3;
+use ndarray::Array4;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -13,28 +13,30 @@ fn parse_state(input: u8) -> Option<bool> {
   }
 }
 
-fn count_neighbors(state: &Array3<bool>) -> Array3<u8> {
-  let mut result: Array3<u8> = Array3::zeros(state.raw_dim());
+fn count_neighbors(state: &Array4<bool>) -> Array4<u8> {
+  let mut result: Array4<u8> = Array4::zeros(state.raw_dim());
   let shape = result.raw_dim();
 
   let bounded_range =
     |d, i| (if i == 0 { 0 } else { -1 as isize })..(if i + 1 == shape[d] { 1 } else { 2 });
 
-  for ((i, j, k), value) in result.indexed_iter_mut() {
-    for (x, y, z) in iproduct!(
+  for ((i, j, k, l), value) in result.indexed_iter_mut() {
+    for (x, y, z, v) in iproduct!(
       bounded_range(0, i),
       bounded_range(1, j),
-      bounded_range(2, k)
+      bounded_range(2, k),
+      bounded_range(3, l)
     ) {
       if state[[
         i.wrapping_add(x as usize),
         j.wrapping_add(y as usize),
         k.wrapping_add(z as usize),
+        l.wrapping_add(v as usize),
       ]] {
         *value += 1;
       }
     }
-    if state[[i, j, k]] {
+    if state[[i, j, k, l]] {
       *value -= 1;
     }
   }
@@ -42,7 +44,7 @@ fn count_neighbors(state: &Array3<bool>) -> Array3<u8> {
   return result;
 }
 
-fn update_state(state: &mut Array3<bool>) {
+fn update_state(state: &mut Array4<bool>) {
   let neighbors = count_neighbors(state);
 
   for (idx, value) in state.indexed_iter_mut() {
@@ -78,22 +80,24 @@ fn main() {
 
   let final_size = (
     1 + 2 * iterations,
+    1 + 2 * iterations,
     1 + input.len() + 2 * iterations,
     1 + input[0].len() + 2 * iterations,
   );
 
-  let mut state: Array3<bool> = Array3::from_elem(final_size, false);
+  let mut state: Array4<bool> = Array4::from_elem(final_size, false);
   for i in 0..input.len() {
     for j in 0..input[0].len() {
       state[[
         final_size.0 / 2,
-        final_size.1 / 2 - input.len() / 2 + i,
-        final_size.2 / 2 - input[0].len() / 2 + j,
+        final_size.1 / 2,
+        final_size.2 / 2 - input.len() / 2 + i,
+        final_size.3 / 2 - input[0].len() / 2 + j,
       ]] = input[i][j];
     }
   }
 
-  let print_state = |i, state: &Array3<bool>| {
+  let print_state = |i, state: &Array4<bool>| {
     println!(
       "cycle {}, {} active cubes",
       i,
