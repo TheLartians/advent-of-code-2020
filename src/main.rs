15a6f7eec7f6d0e04d6f59a0b2911b976add6c17
Lars Melchior <TheLartians@users.noreply.github.com>
2020-12-23 09:46:30 +0100

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -4,76 +4,50 @@ use std::env;
 use std::fs;
 use std::iter::Iterator;
 
-type Scalar = usize;
-
-fn parse_cards(input: &str) -> VecDeque<Scalar> {
-  return input
-    .split('\n')
-    .skip(1)
-    .filter(|s| s.len() > 0)
-    .map(|s| s.parse().unwrap())
-    .collect();
-}
-
-fn recursive_combat(deck1: &mut VecDeque<Scalar>, deck2: &mut VecDeque<Scalar>) -> bool {
-  let mut cache: HashSet<(Vec<Scalar>, Vec<Scalar>)> = HashSet::new();
-
-  while ![&deck1, &deck2].iter().any(|d| d.len() == 0) {
-    let key = (
-      deck1.iter().map(|&v| v).collect(),
-      deck2.iter().map(|&v| v).collect(),
-    );
-    if cache.contains(&key) {
-      deck2.clear();
-    } else {
-      cache.insert(key);
-      let p1 = deck1.pop_front().unwrap();
-      let p2 = deck2.pop_front().unwrap();
-
-      let p1_wins = if deck1.len() >= p1 && deck2.len() >= p2 {
-        recursive_combat(
-          &mut deck1.iter().take(p1).map(|&v| v).collect(),
-          &mut deck2.iter().take(p2).map(|&v| v).collect(),
-        )
-      } else {
-        p1 > p2
-      };
-
-      if p1_wins {
-        deck1.push_back(p1);
-        deck1.push_back(p2);
-      } else {
-        deck2.push_back(p2);
-        deck2.push_back(p1);
-      }
-    }
-  }
-
-  return deck2.len() == 0;
-}
+type Scalar = u8;
 
 fn main() {
   let mut args = env::args();
   args.next();
-  let filename = args.next().unwrap();
-  let (mut deck1, mut deck2) = fs::read_to_string(&filename)
+  let mut cups = args
+    .next()
     .unwrap()
-    .split("\n\n")
-    .map(|s| parse_cards(&s))
-    .next_tuple()
-    .unwrap();
+    .bytes()
+    .map(|v| v - b'0')
+    .collect::<VecDeque<Scalar>>();
 
-  recursive_combat(&mut deck1, &mut deck2);
-
-  println!(
-    "the final score result is {}",
-    [&deck1, &deck2]
+  for i in 0..100 {
+    let current = cups.pop_front().unwrap();
+    let next = cups.drain(0..3).collect::<Vec<_>>();
+    cups.push_back(current);
+    let destination = cups
       .iter()
-      .map(|&v| v)
-      .flatten()
-      .rev()
+      .map(|&v| {
+        if v < current {
+          current - v
+        } else {
+          current + 9 - v
+        }
+      })
       .enumerate()
-      .map(|(i, &v)| (i as Scalar + 1) * v)
-      .fold(0, |a, b| a + b)
+      .sorted_by(|(_, a), (_, b)| Ord::cmp(a, b))
+      .map(|(i, _)| i)
+      .next()
+      .unwrap()
+      + 1;
+    next.iter().rev().for_each(|&v| cups.insert(destination, v));
+    println!("after round {}: {:?}", i+1, cups);
+  }
+
+  cups.rotate_left(
+    cups
+      .iter()
+      .enumerate()
+      .filter(|(_, &v)| v == 1)
+      .map(|(i, _)| i)
+      .next()
+      .unwrap(),
   );
+
+  println!("result: {:?}", String::from_utf8(cups.iter().skip(1).map(|&v| b'0' + v).collect::<Vec<_>>()).unwrap());
 }
